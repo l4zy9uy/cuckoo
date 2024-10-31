ac03f3ab49cb0fd14ba2b617e45d4f76830236e8
l4zy9uy <hoanghuudon02hp@gmail.com>
2024-10-31 18:58:22 +0700

    refactor: make header for table clearly

--- frontend/src/components/HeaderAction.tsx
+++ frontend/src/components/HeaderAction.tsx
@@ -2,14 +2,21 @@
 import React from 'react';
 import {Box, Button, IconButton} from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
+import Typography from "@mui/material/Typography";
 
 const HeaderActions = () => {
     return (
         <Box sx={{
             display: 'flex',
             justifyContent: 'space-between',
-            marginBottom: '1rem'
+            marginBottom: '1rem',
+            alignItems: "center"
         }}>
+            <Typography
+                fontWeight="bold"
+                fontSize="28px"
+
+            > Hang hoa </Typography>
             <Button variant="contained" startIcon={<AddIcon/>}>
                 Thêm mới
             </Button>
