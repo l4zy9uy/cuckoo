9055225c2ae0d814ae9bd3da04efda21547e4992
Vũ Huy Công <huycong892@gmail.com>
2024-11-27 20:59:40 +0700

    feat: add CRUD function for menu manager

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,17 +1,23 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
+import { router } from "./routes";
+import { config } from "./config/config";
 
 const app = express();
 const PORT = 3000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
+app.use(router);
 
+config(app).then(() => {
+  console.log("Config successfully.");
+});
 // A basic route
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, TypeScript + Express!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, TypeScript + Express!");
 });
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
