104cb83bb73bd35587b600c9049a78be9d57730a
l4zy9uy <hoanghuudon02hp@gmail.com>
2024-12-03 20:58:02 +0700

    feat: make router go direct into home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -23,7 +23,7 @@ const App: React.FC = () => {
 
 const WrappedApp: React.FC = () => {
     return (
-        <Router>
+        <Router basename="/cuckoo">
             <App />
         </Router>
     );
